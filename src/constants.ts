757d51f927e6fa96baf3229a0020b29d6ea08fe8
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-05-07 19:45:53 +0600

    added all constants

---
+++ src/constants.ts
@@ -0,0 +1,56 @@
+export const currencyByRupee: Currency[] = [
+  {
+    name: 'DOLLAR',
+    value: 0.0081,
+    flag: '🇺🇸',
+    symbol: '$',
+  },
+  {
+    name: 'EURO',
+    value: 0.0069,
+    flag: '🇪🇺',
+    symbol: '€',
+  },
+  {
+    name: 'POUND',
+    value: 0.006,
+    flag: '🇬🇧',
+    symbol: '£',
+  },
+  {
+    name: 'RUBEL',
+    value: 0.61,
+    flag: '🇷🇺',
+    symbol: '₽',
+  },
+  {
+    name: 'AUS DOLLAR',
+    value: 0.011,
+    flag: '🇦🇺',
+    symbol: 'A$',
+  },
+  {
+    name: 'CAN DOLLAR',
+    value: 0.011,
+    flag: '🇨🇦',
+    symbol: 'C$',
+  },
+  {
+    name: 'YEN',
+    value: 1.27,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: 'DINAR',
+    value: 0.81,
+    flag: '🇰🇼',
+    symbol: 'KD',
+  },
+  {
+    name: 'BITCOIN',
+    value: 0.000000101,
+    flag: '🎰',
+    symbol: '₿',
+  },
+];
